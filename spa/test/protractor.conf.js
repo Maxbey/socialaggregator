42857a6498928c7d92408cd1edc3fedc5ea3a575
Maxim Beiner <mbeiner@crystalnix.com>
2016-09-05 21:45:20 +0600

    Add protractor test for adding facebook account.

--- spa/test/protractor.conf.js
+++ spa/test/protractor.conf.js
@@ -1,5 +1,8 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   framework: 'jasmine2',
-  specs: ['protractor_spec/a.js']
+  specs: [
+  'protractor_spec/facebook_login_test.js',
+  'protractor_spec/login_and_account_add_test.js'
+  ]
 };
