08b5be939e32b0ccc9cdf3a0012b444a48417f9c
mbeiner <mbeiner@crystalnix.com>
2016-09-06 12:38:25 +0600

    Improve docker workflow, configure s3 deploy.
    Combine images for django and celery.
    Configure deploy to aws s3 via gulp.

--- socialaggregator/bin/run.sh
+++ socialaggregator/bin/run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
-
-let port=8000
-
-if  [ -n "$PORT" ]
+if [ "$CONTAINER_BEHAVIOUR" == "DJANGO" ]
   then
-    port=$PORT
-fi
+    python manage.py collectstatic --noinput
+    python manage.py makemigrations
+    python manage.py migrate
 
-gunicorn -w 1 --bind 0.0.0.0:$port app.wsgi
+    let port=8000
+
+    if  [ -n "$PORT" ]
+      then
+        port=$PORT
+    fi
+
+    gunicorn -w 1 --bind 0.0.0.0:$port app.wsgi
+
+elif [ "$CONTAINER_BEHAVIOUR" == "CELERY" ]
+  then
+    celery -A app worker -l info
+fi
